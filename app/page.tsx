de2a8280c10a9befaebf43c05db8664a89371db6
Vishaalcodre <vishaa369@gmail.com>
2024-08-02 01:20:46 +0530

    Bento Grid Layout setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import Image from "next/image";
@@ -13,6 +14,7 @@ export default function Home() {
         navItems={[{name: 'Home', link: '/', 
         icon: <FaHome />}]}/>   
         <Hero />
+        <Grid />
       </div>
     </main>
   );
